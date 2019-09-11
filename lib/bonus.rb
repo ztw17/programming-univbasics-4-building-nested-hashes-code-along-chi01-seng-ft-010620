a16164ba32e7dceb3da2102e61d5f070433bb0f3
Steven G. Harms <steven.harms@flatironschool.com>
2019-09-11 17:07:41 -0400

    Whitespace

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -8,13 +8,13 @@ def bonus
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Mercutio", age: "18", attitude: "hot-headed"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
